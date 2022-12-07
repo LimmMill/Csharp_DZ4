f28d7132f7ef001baec2f767fad9e8d9f08502c4
Name <limpamposha@mail.ru>
2022-12-07 19:55:22 +0300

    задача 27

---
+++ DZ4_zadacha27/Program.cs
@@ -0,0 +1,28 @@
+﻿//  27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+// 452 -> 11    82 -> 10     9012 -> 12
+{
+  Console.Writline("Введите целое число");
+  int num = Convert.ToInt32(Console.ReadLine());
+  int sum = 0;
+  int amount(int num);
+    { 
+    
+       for (int i=num; i >0; i=i /10);
+        {
+          num = i % 10;
+          sum = sum + num;
+        }
+       return sum;
+    }
+    
+    Console.Writline($"{amount(num)}");
+}    
+catch
+{
+    Console.Writline("Ошибка, введите целое число");
+    
+}
+
+
+
+
