4daa8be28e016e98718ae25e7bc990fb15864563
Name <limpamposha@mail.ru>
2022-12-08 01:54:30 +0300

    задача 29

---
+++ DZ4_zadacha29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и 
+// далее выводит массив на экран в одну строку.
+void ChekArray()
+{
+Console.WriteLine("Введите массив из восьми чисел");
+int[] nums = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write($"[{nums[0]},{nums[1]},{nums[2]},{nums[3]},{nums[4]},{nums[5]},{nums[6]},{nums[7]}]");
+}
+ChekArray();
